3311702b6ef80710aeb1c5b3ac4413778f464d60
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-03-16 18:09:03 +0100

    une classe Contact

--- src/app/contactpage/contactpage.component.ts
+++ src/app/contactpage/contactpage.component.ts
@@ -1,5 +1,48 @@
 import { Component } from '@angular/core';
 
+
+export class Contact {
+  firstName: string;
+  lastName: string;
+  email: string;
+  content: string;
+
+
+  constructor(firstName: string, lastName: string, email: string, content: string){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.content = content;
+  }
+
+  createContact(firstName: string, lastName: string, email: string, content: string) : Contact{
+    try {
+      if (this.checkEmail(email)){
+        this.validMail();
+      }
+    }
+    catch(error){
+        this.invalidMail();
+        throw new Error("invalid-email");
+    }
+    return new Contact(firstName, lastName, email, content);
+  }
+  
+  checkEmail(email: string) : boolean{
+    return true;
+  }
+
+  validMail(){
+
+  }
+  invalidMail(){
+
+  }
+
+
+}
+
+
 @Component({
   selector: 'app-contactpage',
   templateUrl: './contactpage.component.html',
@@ -8,3 +51,4 @@ import { Component } from '@angular/core';
 export class ContactpageComponent {
 
 }
+
