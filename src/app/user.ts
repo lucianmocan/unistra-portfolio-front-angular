8055a7220d8f296dbbc85997e79e42ad4870dcc8
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-10 23:54:05 +0200

    user interface -> class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,19 @@
-export interface User {
-    firstName : string;
-    lastName: string;
-    email: string;
-    content: string;
+export class User {
+    firstName : string = "";
+    lastName: string = "";
+    email: string = "";
+    content: string = "";
+    constructor(fname: string, lname: string, email: string, content: string){
+      this.firstName = fname;
+      this.lastName = lname;
+      this.email = email;
+      this.content = content;
+    }
+
+    isEqual(u: User): boolean{
+      if (u.firstName == this.firstName && u.lastName == this.lastName && u.email == this.email && u.content == this.content){
+        return true;
+      }
+      return false;
+    }
   }
