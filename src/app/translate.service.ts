46dff587d1d166f56d781673f16568f83acb3380
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-10 22:25:45 +0200

    translation service and languages json

---
+++ src/app/translate.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import * as fr from "../translations/fr.json";
+@Injectable({
+  providedIn: 'root'
+})
+export class TranslateService {
+
+  constructor() { }
+  languagePack = fr;
+  getLanguage(language: string){
+    import('../translations/'+language+'.json').then((data:any) => {
+      this.languagePack = data;
+    })
+  }
+}
