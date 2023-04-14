b0bfa919090645a9d4fe31807b93a55a9020839e
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-14 21:03:22 +0200

    admin view with better displaying

--- src/app/adminview/adminview.component.ts
+++ src/app/adminview/adminview.component.ts
@@ -3,6 +3,16 @@ import { UserService } from '../user.service';
 import { User } from '../user';
 import { ActivatedRoute } from '@angular/router';
 import { AccessGuardService } from '../access-guard.service';
+
+class Search {
+  value: string;
+  choice: string;
+  constructor(value: string, choice: string){
+    this.value = value;
+    this.choice = choice;
+  }
+}
+
 @Component({
   selector: 'app-adminview',
   templateUrl: './adminview.component.html',
@@ -13,9 +23,24 @@ export class AdminviewComponent {
               private accessGuard: AccessGuardService){}
 
   users : User[] = [];
-
+  
+  search: Search = new Search("", "");
   ngOnInit(){
-    this.userService.getUsers()
-    .subscribe(users => this.users = users);
+  }
+
+  searchAction(){
+    console.log(this.search);
+    if (this.search.choice == 'all'){
+      this.userService.getUsers()
+      .subscribe(
+        users => { this.users = users; }
+      );
+    }
+    else if (this.search.choice == 'id'){
+      this.userService.getUserById(this.search.value)
+      .subscribe(user => {
+        this.users = [user];
+      });
+    }
   }
 }
