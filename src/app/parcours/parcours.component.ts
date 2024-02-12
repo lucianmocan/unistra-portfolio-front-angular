52de1ae360966ff8f0ddbd0d025ddc4dab44296a
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2024-02-13 00:35:19 +0100

    parcours component

---
+++ src/app/parcours/parcours.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parcours',
+  templateUrl: './parcours.component.html',
+  styleUrls: ['./parcours.component.scss']
+})
+export class ParcoursComponent {
+
+}
