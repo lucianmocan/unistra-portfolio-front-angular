42ee08419ad0b151057676b1c3765ebed4117e83
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2024-02-13 11:29:35 +0100

    deploy front-end using Firebase Hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { environment } from './environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -10,6 +10,7 @@ import { ContactpageComponent } from './contactpage/contactpage.component';
 import { FormsModule } from '@angular/forms';
 import { AdminviewComponent } from './adminview/adminview.component';
 import { ParcoursComponent } from './parcours/parcours.component';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ParcoursComponent } from './parcours/parcours.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
